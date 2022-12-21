a7d12be9becb16f734a97f3a09f8d1bd64aff3a4
rebaderbs <rderbak@gmail.com>
2022-12-21 14:55:39 -0600

    Angular frontend working with Java backend

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    //.subscribe to be notified of datat that comes back from server
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
+
+
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: string;
+  imageUrl: string;
+  employeeCode: string;
+
+}
+//this represents the type of data that will be returning, it mirrors what is being shown for each Employee in the backend
